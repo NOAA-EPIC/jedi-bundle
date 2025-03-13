0e9e7587e7b48a11f26b775adc8d04d159830fc5
chan-hoo <chan-hoo.jeon@noaa.gov>
2025-03-13 11:58:21 -0500

    fix module file for orion

--- modulefiles/compile_orion.sh
+++ modulefiles/compile_orion.sh
@@ -2,8 +2,8 @@
 set -eux
 
 HOME_DIR="/work/noaa/epic/UFS_Land-DA_v2.1"
-JEDI_DIR="${HOME_DIR}/jedi_v8_ic_orion"
-BUILD_DIR="${JEDI_DIR}/build"
+JEDI_DIR="${HOME_DIR}/jedi_bundle/modulefiles"
+BUILD_DIR="${HOME_DIR}/build"
 
 module purge
 set +x
